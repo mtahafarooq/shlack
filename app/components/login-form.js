99db687db6f02919f381c5cbaf000f9d337aeec9
Taha <mtahafrq@gmail.com>
2021-12-05 18:10:05 +0100

    add auth service authentication

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,19 +1,20 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object'
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import AuthService from 'shlack/services/auth';
 
 export default class LoginFormComponent extends Component {
     @tracked
     userId = null;
-
+    /**
+     * @type {AuthService}
+     */
+    @service auth;
     get isDisabled() {
         return !this.userId;
     }
     
-    loginAsUserWithId(val) {
-        console.log("userId", val)
-    }
-
     /**
      * 
      * @param {Event & {target: HTMLSelectElement}} evt
@@ -32,7 +33,7 @@ export default class LoginFormComponent extends Component {
         const { target } = evt;
         const val = target.querySelector('select').value
         evt.preventDefault();
-        this.loginAsUserWithId(val)
+        this.auth.loginWithUserId(val);
     }
 }
 
