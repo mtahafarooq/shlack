27f0f5aa456f70e5d0b26751b2fc1960a77f57f2
Taha <mtahafrq@gmail.com>
2021-12-07 16:04:01 +0100

    replace hard coded data with api data

--- app/routes/teams.js
+++ app/routes/teams.js
@@ -1,27 +1,8 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import AuthService from 'shlack/services/auth';
+import fetch from 'fetch';
 
-export const ALL_TEAMS = [
-    {
-        id: "linkedin",
-        name: "LinkedIn",
-        order: 2,
-        iconUrl: "/assets/img/linkedin.png"
-      },
-      {
-        id: "ms",
-        name: "Microsoft",
-        order: 3,
-        iconUrl: "/assets/img/microsoft.png"
-      },
-      {
-        id: "avengers",
-        name: "Avengers",
-        order: 4,
-        iconUrl: "/assets/img/avengers.jpg"
-      }
-]
 export default class TeamsRoute extends Route {
     /**
      * @type {AuthService}
@@ -34,7 +15,8 @@ export default class TeamsRoute extends Route {
         }
     }
 
-    model(){
-        return ALL_TEAMS;
+    async model(){
+        const response = await fetch('/api/teams');
+        return await response.json();
     }
 }
