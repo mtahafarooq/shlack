e1917643e4bd4a22574785bc1917eb4883720b71
Taha <mtahafrq@gmail.com>
2021-12-07 16:27:12 +0100

    add transition to select first team and channel

--- app/routes/teams/team/channel.js
+++ app/routes/teams/team/channel.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 import fetch from 'fetch';
 
 export default class TeamsTeamChannelRoute extends Route {
+    
     async model({channelId}){
 
         const { teamId } = this.paramsFor('teams.team');
