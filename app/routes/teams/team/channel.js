0375d6e7122fd389b7d5362eb9036c56d5ff84f0
Taha <mtahafrq@gmail.com>
2021-12-08 11:36:18 +0100

    add container and prsentational component

--- app/routes/teams/team/channel.js
+++ app/routes/teams/team/channel.js
@@ -4,10 +4,8 @@ import fetch from 'fetch';
 export default class TeamsTeamChannelRoute extends Route {
     
     async model({channelId}){
-
         const { teamId } = this.paramsFor('teams.team');
         const response = await fetch(`/api/teams/${teamId}/channels/${channelId}`)
         return await response.json();
-
     }
 }
